0f9f10b241f885b6ced115aecbc8095e5bdc0243
Maximilian Gerhardt <max.gerhardt@physec.de>
2021-08-06 15:38:59 +0200

    Add initial files

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sam3x8e.h>
+#include <component/pio.h>
+
+int main() {
+  //blackmagic-incantation for PIO (basically GPIO here) blinky
+  //this is untested, probably not working -- just to illustrate that
+  //peripherals are accessible.
+  //PA0 as output (output enable register)
+  PIOA->PIO_OER |= (1 << 0);
+
+  return 0;
+}
